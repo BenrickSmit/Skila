3584424f832b144ca7623e759539bdd0eafa00e9
MetatronicProgramming <metatronicprogramming@hotmail.com>
2021-01-07 07:20:19 +0800

    Removed Unnecessary Files

--- out/build/x64-Debug (default)/lib/googletest-release-1.10.0/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gary/source/repos/BenrickSmit/Skila/lib/googletest-release-1.10.0/googletest
-# Build directory: C:/Users/Gary/source/repos/BenrickSmit/Skila/out/build/x64-Debug (default)/lib/googletest-release-1.10.0/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug (default)/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Gary/source/repos/BenrickSmit/Skila/tests
-# Build directory: C:/Users/Gary/source/repos/BenrickSmit/Skila/out/build/x64-Debug (default)/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
