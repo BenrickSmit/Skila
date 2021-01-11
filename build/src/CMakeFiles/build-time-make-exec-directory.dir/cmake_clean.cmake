6d07222ecd0f2cdb3fc204cf1ccaecb089e4b5b0
MetatronicProgramming <metatronicprogramming@hotmail.com>
2021-01-11 16:41:09 +0800

    Remove unnecessary build dir

--- build/src/CMakeFiles/build-time-make-exec-directory.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/build-time-make-exec-directory"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/build-time-make-exec-directory.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gary/Documents/Coding/C++/Skila/tests
-# Build directory: /home/gary/Documents/Coding/C++/Skila/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
