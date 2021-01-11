ab20f9b1842d84bca889ce6979a93d26066cbc9c
MetatronicProgramming <metatronicprogramming@hotmail.com>
2021-01-11 15:55:20 +0800

    Add Release/Debug application choice

--- run_application.sh
+++ run_application.sh
@@ -7,9 +7,25 @@
 
 ## Read the filename from the chosen location
 SOURCE_FILENAME="${PWD}/build_info/build_name.txt"
+RELEASE_TYPE="${PWD}/build_info/build_type.txt"
+DEBUG_PREFIX="${PWD}/build_info/build_debug_prefix.txt"
+
+## Read the filename
 EXEC_FILENAME=(`cat $SOURCE_FILENAME`)
 EXEC_FILENAME=${EXEC_FILENAME}
 
+## Read the debug prefix
+RELEASE_TYPE=(`cat ${RELEASE_TYPE}`)
+
+## Read whether its a release version or debug version
+DEBUG_PREFIX=(`cat ${DEBUG_PREFIX}`)
+
 ## Change directory to execute the file
 cd build/src
+if [ ${RELEASE_TYPE} = "Release" ] || [ ${RELEASE_TYPE} = "release" ]; then
+echo "Running Release Application: " ${EXEC_FILENAME}
 ./${EXEC_FILENAME}
+else
+echo "Running Debug Application: " ${EXEC_FILENAME}-${DEBUG_PREFIX}
+./${EXEC_FILENAME}-${DEBUG_PREFIX}
+fi
